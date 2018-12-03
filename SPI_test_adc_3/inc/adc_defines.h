73ebfa3e5da3d2247e6e1d2c8497137a14e9f4ff
SamSolondz <45467940+SamSolondz@users.noreply.github.com>
2018-12-03 14:19:22 -0700

    Fixed the header files and includes.
    Could be cleaned up a bit

---
+++ SPI_test_adc_3/inc/adc_defines.h
@@ -0,0 +1,9 @@
+#define COMMS_ADDR = 0x00
+#define COMMS_READ_ID = 0x07
+
+
+#define ID_VAL 0x4fd0 //(0x4FDX)
+#define ID_VAL3 0x4 //(0x4FDX)
+#define ID_VAL2 0xf //(0x4FDX)
+#define ID_VAL1 0xd //(0x4FDX)
+#define ID_ADDR 0x07
